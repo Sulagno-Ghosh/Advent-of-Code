6470a2df65e72eeb9382739a70d0842dbc587a97
Sulagno Ghosh <sulagnoghosh889@gmail.com>
2023-12-21 16:34:02 +0530

    changing

--- day1.py
+++ day1.py
@@ -1,34 +1,6 @@
 f = open("input.txt", "r")
 lines = f.readlines()
-list = []
 
-def forwardfunc(s):
-  for i in range(len(s)):
-    if s[i].isdigit():
-      return str(s[i])
-
-
-def backwordfunc(s):
-  for i in range(len(s)-1,-1,-1):
-    if s[i].isdigit():
-      return str(s[i])
-
-
-
-for i in range(len(lines)):
-    result = 0
-    a = forwardfunc(lines[i])
-    b = backwordfunc(lines[i])
-    if result == None:
-        list.append(0)
-
-    result = a + b
-
-    list.append(int(result))
-
-    result = sum(list)
-
-
-
-print(result)
+for i in lines:
+    print(i)
 
---
+++ go.mod
@@ -0,0 +1,3 @@
+module Advent-of-Code
+
+go 1.21.5
